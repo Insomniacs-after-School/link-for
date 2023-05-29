8756ab38f5ed269159646d4efde29e3c0697e62e
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-05-29 21:17:43 +0700

    fix model

--- src/model/User.js
+++ src/model/User.js
@@ -65,4 +65,47 @@ if (User === sequelize.models.User) {
   console.log('tabel user berhasil dibuat');
 }
 
-module.exports = User;
+const Datas = sequelize.define(
+  'Datas',
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.CHAR(36),
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    link: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.STRING,
+      defaultValue: new Date().toDateString(),
+    },
+    updatedAt: {
+      type: DataTypes.STRING,
+      defaultValue: new Date().toDateString(),
+    },
+  },
+);
+
+// // the defined model is the class itself
+if (Datas === sequelize.models.Datas) {
+  Datas.sync();
+  console.log('tabel data berhasil dibuat');
+}
+
+module.exports = { User, Datas };
